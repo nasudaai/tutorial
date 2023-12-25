0bd7ee6e52004e10bdf871686a56b41e76cb6e51
nasuda <nasuda908@gmail.com>
2023-12-26 07:32:04 +0900

    add code

--- reacr-router-tutorial/src/routes/root.tsx
+++ reacr-router-tutorial/src/routes/root.tsx
@@ -2,8 +2,14 @@ import {
   Outlet,
   Link,
   useLoaderData,
+  Form,
   } from 'react-router-dom'
-import { getContacts } from '../contacts'
+import { getContacts, createContact } from '../contacts'
+
+export async function action() {
+  const contact = await createContact()
+  return { contact }
+}
 
 
 export async function loader() {
@@ -39,9 +45,9 @@ export default function Root() {
               aria-live='polite'
             ></div>
           </form>
-          <form method='post'>
+          <Form method='post'>
             <button type="submit">New</button>
-          </form>
+          </Form>
         </div>
         <nav>
           {contacts.length ? (
